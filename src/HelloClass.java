b7e7dd009fa99e1b673a3e6acbf2bef40d512f48
VanPark <vanpark212@gmail.com>
2023-09-22 09:41:41 -0700

    Done

---
+++ src/HelloClass.java
@@ -0,0 +1,6 @@
+public class HelloClass {
+    public static void main(String args[]){
+        System.out.println("Hello Joydeep Mukherje.");
+        System.out.println("This is my proof that I know how to use IntelliJ");
+    }
+}
