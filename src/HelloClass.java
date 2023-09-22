c2371e72dce63b574d0b599f272d9707aacab3d5
VanPark <vanpark212@gmail.com>
2023-09-22 09:42:33 -0700

    Fixed Typo

--- src/HelloClass.java
+++ src/HelloClass.java
@@ -1,6 +1,6 @@
 public class HelloClass {
     public static void main(String args[]){
-        System.out.println("Hello Joydeep Mukherje.");
+        System.out.println("Hello Joydeep Mukherjee.");
         System.out.println("This is my proof that I know how to use IntelliJ");
     }
 }
